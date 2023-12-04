978513137c2a217efaed8e55032ccfbc954f13d7
Elif Demirtaş <elif_demirtas_@outlook.com>
2023-12-04 15:51:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+       KronometreThread thread1=new KronometreThread("thread1");
+        KronometreThread thread2=new KronometreThread("thread2");
+       thread1.start();
+       thread2.start();
+    }
+}
